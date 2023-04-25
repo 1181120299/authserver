5af6a293846d2080dba4df094065729f80d7c591
Jack <1181120299@qq.com>
2023-04-25 19:22:22 +0800

    refactor: OAuth client调整为不需要授权确认页面

--- src/main/java/com/jack/authserver/service/impl/Oauth2RegisteredClientServiceImpl.java
+++ src/main/java/com/jack/authserver/service/impl/Oauth2RegisteredClientServiceImpl.java
@@ -23,6 +23,7 @@ public class Oauth2RegisteredClientServiceImpl extends ServiceImpl<Oauth2Registe
             throw new RRException("应用名称已存在");
         }
 
+        boolean requireAuthorizationConsent = false;
         entity.setRedirectUris(generateRedirectUris(entity.getRedirectUriSimple()));
         entity.setClientIdIssuedAt(new Date());
         entity.setClientSecret("{noop}secret");
@@ -30,7 +31,7 @@ public class Oauth2RegisteredClientServiceImpl extends ServiceImpl<Oauth2Registe
         entity.setClientAuthenticationMethods("client_secret_basic");
         entity.setAuthorizationGrantTypes("refresh_token,client_credentials,authorization_code");
         entity.setScopes("openid,profile,message.read,message.write");
-        entity.setClientSettings("{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":true}");
+        entity.setClientSettings("{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":" + requireAuthorizationConsent + "}");
         entity.setTokenSettings("{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000]}");
 
         return baseMapper.insert(entity) > 0;
