2c2b60931331caad260cd52b71222c293a709a19
Jack <1181120299@qq.com>
2023-04-26 22:13:10 +0800

    refactor: LoginController增加条件注入

--- src/main/java/com/jack/authserver/controller/LoginController.java
+++ src/main/java/com/jack/authserver/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.jack.authserver.controller;
 import com.jack.authserver.annotation.LoginEntryProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@ConditionalOnMissingBean(LoginEntryProvider.class)
 public class LoginController implements LoginEntryProvider {
 
     @Override
